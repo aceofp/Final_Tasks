50da5206e73022e7f16096f3cce28f392d122199
kermit <aceofp@gmail.com>
2022-04-05 16:00:24 +0200

    All done in Task1

--- src/test/java/Task_1/TestRunner1.java
+++ src/test/java/Task_1/TestRunner1.java
@@ -1,12 +1,11 @@
-package TestRunner1;
+package Task_1;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/Cucumber/Features/Task_1.feature", plugin = {"pretty", "html:out"},
-        glue = "Task_1")
+@CucumberOptions(features = "src/Cucumber/Features/Task_1.feature", plugin = {"pretty", "html:out"})
 
 public class TestRunner1 {
 
